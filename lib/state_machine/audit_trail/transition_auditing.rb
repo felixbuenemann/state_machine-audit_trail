331dcdf6e6b3154728e674c6912cd52a6f5a2318
Felix Bünemann <buenemann@louis.info>
2011-09-25 20:05:40 +0200

    Store related user on state_machine

--- lib/state_machine/audit_trail/transition_auditing.rb
+++ lib/state_machine/audit_trail/transition_auditing.rb
@@ -1,23 +1,32 @@
 module StateMachine::AuditTrail::TransitionAuditing
-  attr_accessor :transition_class_name
+  attr_accessor :transition_class_name, :user
 
   def store_audit_trail(options = {})
     state_machine = self
+    state_machine.user = options[:user]
     state_machine.transition_class_name = (options[:to] || default_transition_class_name).to_s
 
     state_machine.after_transition do |object, transition|
-      state_machine.audit_trail.log(object, options[:user], transition.event, transition.from, transition.to)
+      state_machine.audit_trail.log(object, state_machine.user, transition.event, transition.from, transition.to)
     end
 
     state_machine.owner_class.after_create do |object|
       if !object.send(state_machine.attribute).nil?
-        state_machine.audit_trail.log(object, options[:user], nil, nil, object.send(state_machine.attribute))
+        state_machine.audit_trail.log(object, state_machine.user, nil, nil, object.send(state_machine.attribute))
       end
     end
   end
 
   def audit_trail
     @transition_auditor ||= StateMachine::AuditTrail.create(transition_class)
+  end
+
+  def user=(user)
+    @user = user
+  end
+
+  def user
+    @user
   end
 
   private
